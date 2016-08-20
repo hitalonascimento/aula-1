c4ee39e392a30d3b6a5a7d10d3354cd7e627631c
Hitalo Nascimento <hitalo.nascimento@yahoo.com.br>
2016-08-20 18:02:10 -0300

    Fim de aula

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-Hello World!
+<?php
+
+$variavel = "Hello World!"
+
+
+ ?>
+<!DOCTYPE html>
+<html>
+<?php
+
+require_once "layout/head.php";
+
+require_once "layout/body.php";
+?>
+</html>
---
+++ json.php
@@ -0,0 +1,5 @@
+<?php
+
+echo json_encode("Hitalo Nascimento");
+
+ ?>
